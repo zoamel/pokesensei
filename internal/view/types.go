9269cd25baeafdb5e4b241191e9b0f4c4a925b2a
Marcin Wojtczak <mar.wojtczak@gmail.com>
2026-03-16 10:22:58 +0100

    feat: trim PokemonDetail struct and delete dead encounter/move helpers

--- internal/view/types.go
+++ internal/view/types.go
@@ -30,12 +30,9 @@ type PokemonListItem struct {
 
 // PokemonDetail holds all data for the Pokémon detail page.
 type PokemonDetail struct {
-	Pokemon    generated.Pokemon
-	Types      []TypeInfo
-	Encounters []generated.ListEncountersByPokemonRow
-	EvoChain   []generated.GetEvolutionChainByPokemonRow
-	Abilities  []generated.ListPokemonAbilitiesRow
-	Moves      []generated.ListPokemonMovesRow
+	Pokemon   generated.Pokemon
+	Types     []TypeInfo
+	Abilities []generated.ListPokemonAbilitiesRow
 }
 
 // TeamSlotData holds data for a single team slot display.
@@ -52,54 +49,6 @@ type CoverageCell struct {
 	FactorClass  string // "super-effective", "not-effective", "immune", "neutral"
 }
 
-var encounterMethods = map[string]string{
-	"walk":           "Walking",
-	"surf":           "Surfing",
-	"fish-old-rod":   "Old Rod",
-	"old-rod":        "Old Rod",
-	"fish-good-rod":  "Good Rod",
-	"good-rod":       "Good Rod",
-	"fish-super-rod": "Super Rod",
-	"super-rod":      "Super Rod",
-	"gift":           "Gift",
-	"headbutt":       "Headbutt",
-	"rock-smash":     "Rock Smash",
-}
-
-func formatEncounterMethod(s string) string {
-	if formatted, ok := encounterMethods[s]; ok {
-		return formatted
-	}
-	return titleCase(s)
-}
-
-var learnMethods = map[string]string{
-	"level-up": "Level Up",
-	"machine":  "TM/HM",
-	"tutor":    "Tutor",
-	"egg":      "Egg",
-}
-
-func formatLearnMethod(s string) string {
-	if formatted, ok := learnMethods[s]; ok {
-		return formatted
-	}
-	return titleCase(s)
-}
-
-var learnMethodTips = map[string]string{
-	"level-up": "Learned automatically when the Pokémon reaches this level",
-	"machine":  "Taught using a Technical Machine (TM) or Hidden Machine (HM) item",
-	"tutor":    "Taught by a special move tutor NPC in the game",
-	"egg":      "Inherited from a parent Pokémon when hatched from an egg",
-}
-
-func learnMethodTip(s string) string {
-	if tip, ok := learnMethodTips[s]; ok {
-		return tip
-	}
-	return ""
-}
 
 func titleCase(s string) string {
 	parts := strings.Split(s, "-")
